5695a075703afa3d1017b719282623f27a45e8dd
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2026-05-28 10:25:13 +0200

    Continuing the troubleshooting

--- workflow/scripts/demultiplexer.R
+++ workflow/scripts/demultiplexer.R
@@ -51,7 +51,6 @@ loader <- function(state) {
         state$istream <- ShortRead::FastqStreamer(
                 input_file, n = chunk_size
                 )
-        message("Hallo!")
     }
     chunk <- ShortRead::yield(state$istream)  |> ShortRead::sread()
     n_reads_in_chunk <- length(chunk)
@@ -74,6 +73,7 @@ loader <- function(state) {
     state$total_reads <- state$total_reads + n_reads_in_chunk
     chunk_without_adapter <- Biostrings::vcountPattern(bc3_to_bc2_adapter, chunk, max.mismatch = 1L) |> (\(x) chunk[x == 0L])()
     state$reads_without_adapter  <- state$reads_without_adapter + length(chunk_without_adapter)
+    message("Hallo!")
     list(
         state = state,
         sequences = chunk_without_adapter,
