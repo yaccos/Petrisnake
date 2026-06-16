f09184f3af3d75a675cc43f00e67afbb2e699898
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2026-06-16 12:37:14 +0200

    Updated the benchmark running file for the specific purpose of the S. aureus dataset from Yan et al.

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -1,14 +1,14 @@
 script_dir=$(pwd)/../scripts
-sample=EC_3h_deep
-sample_full=EC_3h_deep_S5
+sample=SA_9h_deep
+sample_full=SA_9h_deep_S5
 bc_cutoff=60000
-annotation_dir=GCF_000005845.2
+annotation_dir=GCF_000756205.1
 
 # Makes sure the cutadapt jobs can run in parallel
-cutadapt -g NNNNNNNNAGAATACACGACGCTCTTCCGATCT --cores 3 -o ${sample}/${sample_full}_L001_R1_001.fastq.gz ${sample}/SRR28148450_1.fastq &
-cutadapt -g NNNNNNNNAGAATACACGACGCTCTTCCGATCT --cores 3 -o ${sample}/${sample_full}_L001_R2_001.fastq.gz ${sample}/SRR28148450_2.fastq &
+cutadapt -g NNNNNNNNAGAATACACGACGCTCTTCCGATCT --cores 3 -o ${sample}/${sample_full}_L001_R1_001.fastq.gz ${sample}/SRR28148452_1.fastq &
+cutadapt -g NNNNNNNNAGAATACACGACGCTCTTCCGATCT --cores 3 -o ${sample}/${sample_full}_L001_R2_001.fastq.gz ${sample}/SRR28148452_2.fastq &
 wait
 
 python $script_dir/sc_pipeline_15_generic_v2.py ${sample_full} 1
 chmod +x $script_dir/pipeline_v2_generic.sh
-$script_dir/pipeline_v2_generic.sh ${sample} $bc_cutoff GCF_000005845.2/GCF_000005845.2_ASM584v2_genomic.fna  GCF_000005845.2/genomic.gtf ${sample}
+$script_dir/pipeline_v2_generic.sh ${sample} $bc_cutoff GCF_000756205.1/GCF_000756205.1_ASM75620v1_genomic.fna  GCF_000756205.1/genomic.gtf ${sample}
